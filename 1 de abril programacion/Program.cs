fd13d15e99e126483720808ca7170d39adba4cb8
Dayana Escobar <dorellanae2@miumg.edu.gt>
2023-04-01 09:45:08 -0600

    Agregar archivos de proyecto.

---
+++ 1 de abril programacion/Program.cs
@@ -0,0 +1,47 @@
+﻿void matriz()
+{
+    String[] nombre = new string[]
+    {
+        "Anderson","Brandon","Yeimi","Fatima","Vivian"
+    };
+
+    foreach ( string n in nombre)
+    {
+        Console.WriteLine("nombre=" + n);
+    }
+    int[] notas = new int[]
+    {
+        51,80,96,76,61
+    };
+    int promedio = 0;
+    foreach (int n in notas)
+    {
+        promedio = promedio + n;
+    }
+    promedio = promedio / notas.Length;
+    Console.WriteLine("El promedio es= " + promedio);
+}
+//matriz();
+void buscaMayor()
+{
+    string nombre;
+   {
+        "Anderson"; "Brandon"; "Yeimi"; "Fatima"; "Vivian";
+   };
+    int notas;
+    {
+        51, 80, 96, 76, 61,
+    };
+  
+ 
+    for (int i = 0; i< 5 ; i++)
+    {
+      
+       
+            nombre = notas;
+        
+    }
+    Console.WriteLine("El nota mayor es " + notas );
+    Console.WriteLine("la nota es de la alumna " + nombre);
+ }
+buscaMayor();
